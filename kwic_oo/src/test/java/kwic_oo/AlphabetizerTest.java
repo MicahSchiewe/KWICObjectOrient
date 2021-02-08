29060bb39dcb4073c78848f8879a4931e463871a
Micah <micah_schiewe1@baylor.edu>
2021-02-07 22:30:21 -0600

    Align with specifications in syllabus
    * I'm not sure if they apply to this project, but better safe than sorry
    * Not all tests yet, just getting these up as an insurance policy

---
+++ kwic_oo/src/test/java/kwic_oo/AlphabetizerTest.java
@@ -0,0 +1,40 @@
+package kwic_oo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import ecs.baylor.edu.cs5352.kwic_oo.impl.Alphabetizer;
+import ecs.baylor.edu.cs5352.kwic_oo.impl.Lines;
+
+class AlphabetizerTest {
+
+	@ParameterizedTest
+	@MethodSource("provider")
+	void test(List<String> lines, List<String> results) {
+		Alphabetizer a = new Alphabetizer();
+		Lines l = new Lines(lines);
+		a.alphabetize(l);
+		assertEquals(l.getLines(), results);
+	}
+
+	public static Stream<Arguments> provider() {
+		return Stream.of(Arguments.of(asList("b", "a", "c"), List.of("a", "b", "c")),
+				Arguments.of(asList(), List.of()),
+				Arguments.of(asList("ab", "aa"), List.of("aa", "ab")),
+				Arguments.of(asList("a", "b", "c"), List.of("a", "b", "c")));
+	}
+
+	private static List<String> asList(String...strings) {
+		List<String> list = new ArrayList<>();
+		for (String s : strings)
+			list.add(s);
+		return list;
+	}
+}
---
+++ kwic_oo/src/test/java/kwic_oo/CircularShiftTest.java
@@ -0,0 +1,42 @@
+package kwic_oo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import ecs.baylor.edu.cs5352.kwic_oo.impl.CircularShift;
+import ecs.baylor.edu.cs5352.kwic_oo.impl.Lines;
+
+class CircularShiftTest {
+
+	@ParameterizedTest
+	@MethodSource("provider")
+	void test(List<String> lines, List<String> results) {
+		CircularShift cs = new CircularShift();
+		Lines l = cs.shiftInput(new Lines(lines));
+		l.getLines().sort(null);
+		results.sort(null);
+		assertEquals(l.getLines(), results);
+	}
+
+	public static Stream<Arguments> provider() {
+		return Stream.of(Arguments.of(asList(""), asList("")), Arguments.of(asList(), asList()),
+				Arguments.of(asList("Hello world!", "This is a test", "mahboi"),
+						asList("Hello world!", "world! Hello", "This is a test", "is a test This", "a test This is",
+								"test This is a", "mahboi")),
+				Arguments.of(asList("a", "b", "c"), asList("a", "b", "c")));
+	}
+
+	private static List<String> asList(String... strings) {
+		List<String> list = new ArrayList<>();
+		for (String s : strings)
+			list.add(s);
+		return list;
+	}
+}
